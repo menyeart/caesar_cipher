f2f5d377e914169e7ad6c30cef362976f194d91d
Matt Enyeart <menyeart1@gmail.com>
2022-11-08 13:52:21 -0700

    Add basic summary and pseudocode

---
+++ cipher.rb
@@ -0,0 +1,11 @@
+#Summary. Creating the caesar cipher by converting plain text to integers and then shifting them to other intengers based on a key.
+
+# take a string of letters
+#convert it to integers
+#shift it several places based on the key
+#re convert it to a string of letters
+
+
+def caesar_cipher(string, key)
+  #Code
+end
