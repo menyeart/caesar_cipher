3501f91dd66914388c7fd57b65f98d04dc779b78
Matt Enyeart <menyeart1@gmail.com>
2022-11-09 09:49:20 -0700

    Add code to encrypt a string

--- cipher.rb
+++ cipher.rb
@@ -5,8 +5,30 @@
 #convert it to integers
 #shift it several places based on the key
 #re convert it to a string of letters
-Array.new(25, {a})
 
-def caesar_cipher(string, key)
-  #Code
+# Creating an alphabet hash
+alphabet = {}
+('a'..'z').to_a.each_with_index do |index, letter|
+  alphabet[index] = letter
 end
+p alphabet
+
+# def caesar_encript(string, key)
+key = 3
+string = 'matt'
+number_string =  string.split("").map do |letter|
+    letter = alphabet[letter] + key
+  end
+
+p number_string
+
+encrypted_array = []#convert back to String
+number_string.each do |number|
+  alphabet.select do |key, value|
+    if number == value
+      encrypted_array << key
+    end
+  end
+end
+
+p encrypted_array.join
